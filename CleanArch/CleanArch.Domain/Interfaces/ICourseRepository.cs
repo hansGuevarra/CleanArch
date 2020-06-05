d8ab9d7388c66892ea6981a2611d5c136960349f
Hans Guevarra <hans.guevarra@gmail.com>
2020-06-05 21:05:10 +0800

    Commit Server and Repo and vimodels

---
+++ CleanArch/CleanArch.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+
+
+    }
+}
